cc91f28e09069b13ac86af066c33ae826dde6d6b
Shashank17sahu <shashank17sahu@gmail.com>
2021-07-17 10:11:15 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Square from './Square';
 
 test('renders learn react link', () => {
   render(<App />);
---
+++ src/Components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '/vercel.svg';
+
+export default function Footer() {
+     return (
+        <div>
+            <footer>
+                <a
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                 Powered by{' '}
+                <img src="/vercel.svg" alt="Vercel" className="logo" />
+                </a>
+            </footer>  
+        </div>
+    )
+}
